033dbb8fccaa5cec603ec46620438adbe62bb4c2
salaboy <salaboy@gmail.com>
2020-02-06 15:41:17 +0000

    some refactoring

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# spring-boot-starter-webflux-cloudevents
-Simple Helpers for CloudEvents in Spring Boot WebFlux
+# Spring Boot Starter WebFlux CloudEvents decorator
+Simple Helper for CloudEvents in Spring Boot WebFlux. 
+This brings the `io.cloudevents:cloudevents-api` Java API and reads from HTTP Headers to build CloudEvent objects.
+This depends on  `org.springframework.boot:spring-boot-starter-webflux` WebClient 
--- src/main/java/com/salaboy/cloudevents/helper/CloudEventsHelper.java
+++ src/main/java/com/salaboy/cloudevents/helper/CloudEventsHelper.java
@@ -7,32 +7,59 @@ import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.net.URI;
+import java.time.ZonedDateTime;
 import java.util.Map;
 
 public class CloudEventsHelper {
 
-    public static CloudEvent<AttributesImpl, String> parseFromRequest(Map<String, String> headers, Object body) {
+    public static final String CE_ID = "Ce-Id";
+    public static final String CE_TYPE = "Ce-Type";
+    public static final String CE_SOURCE = "Ce-Source";
+    public static final String CE_SPECVERSION = "Ce-Specversion";
+    public static final String CE_TIME = "Ce-Time";
+
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String CONTENT_TYPE = "Content-Type";
+
+
+    public static CloudEvent<AttributesImpl, String> parseFromRequest(Map<String, String> headers, Object body) throws IllegalStateException {
+        if (headers.get(CE_ID) == null || (headers.get(CE_SOURCE) == null || headers.get(CE_TYPE) == null)) {
+            throw new IllegalStateException("Cloud Event required fields are not present.");
+        }
+
         return CloudEventBuilder.<String>builder()
-                .withId(headers.get("Ce-Id"))
-                .withType(headers.get("Ce-Type"))
-                .withSource((headers.get("Ce-Source") != null) ? URI.create(headers.get("Ce-Source")) : null)
+                .withId(headers.get(CE_ID))
+                .withType(headers.get(CE_TYPE))
+                .withSource((headers.get(CE_SOURCE) != null) ? URI.create(headers.get(CE_SOURCE)) : null)
+                .withTime((headers.get(CE_TIME) != null) ? ZonedDateTime.parse(headers.get(CE_TIME)) : null)
                 .withData((body != null) ? body.toString() : "")
-                .withDatacontenttype((headers.get("Content-Type") != null) ? headers.get("Content-Type") : "application/json")
+                .withDatacontenttype((headers.get(CONTENT_TYPE) != null) ? headers.get(CONTENT_TYPE) : APPLICATION_JSON)
                 .build();
     }
 
     public static WebClient.ResponseSpec createPostCloudEvent(WebClient webClient, CloudEvent<AttributesImpl, String> cloudEvent) {
-        WebClient.RequestBodySpec uri = webClient.post().uri("");
+        return createPostCloudEvent(webClient, cloudEvent);
+    }
+
+    public static WebClient.ResponseSpec createPostCloudEvent(WebClient webClient, String uriString, CloudEvent<AttributesImpl, String> cloudEvent) {
+        WebClient.RequestBodySpec uri = webClient.post().uri(uriString);
         WebClient.RequestHeadersSpec<?> headersSpec = uri.body(BodyInserters.fromValue(cloudEvent.getData()));
         AttributesImpl attributes = cloudEvent.getAttributes();
         WebClient.RequestHeadersSpec<?> header = headersSpec
-                .header("Ce-Id", attributes.getId())
-                .header("Ce-Specversion", attributes.getSpecversion())
-                .header("Content-Type", "application/json")
-                .header("Ce-Type", attributes.getType())
-                .header("Ce-Source", (attributes.getSource() != null) ? attributes.getSource().toString() : "");
+                .header(CE_ID, attributes.getId())
+                .header(CE_SPECVERSION, attributes.getSpecversion())
+                .header(CONTENT_TYPE, APPLICATION_JSON)
+                .header(CE_TYPE, attributes.getType())
+                .header(CE_TIME, (attributes.getTime().isPresent()) ? attributes.getTime().get().toString() : "")
+                .header(CE_SOURCE, (attributes.getSource() != null) ? attributes.getSource().toString() : "");
 
         return header.retrieve();
     }
+
+
+
+
+
+    //@TODO: create a print CLOUD EVENT helper
 
 }

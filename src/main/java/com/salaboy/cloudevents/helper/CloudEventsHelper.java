e73464266671663801fc4740397ffb897634c342
salaboy <salaboy@gmail.com>
2020-04-07 11:31:03 +0100

    adding extensions

--- src/main/java/com/salaboy/cloudevents/helper/CloudEventsHelper.java
+++ src/main/java/com/salaboy/cloudevents/helper/CloudEventsHelper.java
@@ -1,6 +1,7 @@
 package com.salaboy.cloudevents.helper;
 
 import io.cloudevents.CloudEvent;
+import io.cloudevents.extensions.ExtensionFormat;
 import io.cloudevents.v03.AttributesImpl;
 import io.cloudevents.v03.CloudEventBuilder;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -24,23 +25,32 @@ public class CloudEventsHelper {
 
 
     public static CloudEvent<AttributesImpl, String> parseFromRequest(Map<String, String> headers, Object body) throws IllegalStateException {
+        return parseFromRequestWithExtension(headers, body, null);
+    }
+
+
+    public static CloudEvent<AttributesImpl, String> parseFromRequestWithExtension(Map<String, String> headers, Object body, ExtensionFormat ef){
         if (headers.get(CE_ID) == null || (headers.get(CE_SOURCE) == null || headers.get(CE_TYPE) == null)) {
             throw new IllegalStateException("Cloud Event required fields are not present.");
         }
 
-        return CloudEventBuilder.<String>builder()
+        CloudEventBuilder<String> builder = CloudEventBuilder.<String>builder()
                 .withId(headers.get(CE_ID))
                 .withType(headers.get(CE_TYPE))
                 .withSource((headers.get(CE_SOURCE) != null) ? URI.create(headers.get(CE_SOURCE)) : null)
                 .withTime((headers.get(CE_TIME) != null) ? ZonedDateTime.parse(headers.get(CE_TIME)) : null)
                 .withData((body != null) ? body.toString() : "")
                 .withSubject(headers.get(CE_SUBJECT))
-                .withDatacontenttype((headers.get(CONTENT_TYPE) != null) ? headers.get(CONTENT_TYPE) : APPLICATION_JSON)
-                .build();
+                .withDatacontenttype((headers.get(CONTENT_TYPE) != null) ? headers.get(CONTENT_TYPE) : APPLICATION_JSON);
+
+        if(ef != null){
+            builder = builder.withExtension(ef);
+        }
+        return  builder.build();
     }
 
     public static WebClient.ResponseSpec createPostCloudEvent(WebClient webClient, CloudEvent<AttributesImpl, String> cloudEvent) {
-        return createPostCloudEvent(webClient, cloudEvent);
+        return createPostCloudEvent(webClient,"", cloudEvent);
     }
 
     public static WebClient.ResponseSpec createPostCloudEvent(WebClient webClient, String uriString, CloudEvent<AttributesImpl, String> cloudEvent) {
